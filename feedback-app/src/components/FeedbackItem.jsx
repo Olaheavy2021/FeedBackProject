4dda729a663d2a344655213e226883fafa471adb
Sunkanmi Olawuwo <solawuwo@axxess.com>
2022-02-22 19:14:54 +0100

    Forms,validation and animation

--- feedback-app/src/components/FeedbackItem.jsx
+++ feedback-app/src/components/FeedbackItem.jsx
@@ -1,6 +1,7 @@
-import {useState} from 'react'
-
-function FeedbackItem({item}) {
+import Card from "./shared/Card"
+import PropTypes from 'prop-types'
+import {FaTimes} from 'react-icons/fa'
+function FeedbackItem({item,handleDelete}) {
     //component level state
     // const[rating,setRating] = useState(7)
     // const[text,setText] = useState('This is an example of a feedback item')
@@ -12,12 +13,19 @@ function FeedbackItem({item}) {
     // }
 
   return (
-    <div className="card">
+    <Card>
       <div className="num-display">{item.rating}</div>
+      <button onClick={() => handleDelete(item.id)} className="close">
+        <FaTimes color='purple'/>
+      </button>
       <div className="text-display">{item.text}</div>
       {/* <button onClick={handleClick}>Click</button> */}
-    </div>
+    </Card>
   )
+}
+
+FeedbackItem.prototypes = {
+  item: PropTypes.object.isRequired
 }
 
 export default FeedbackItem
